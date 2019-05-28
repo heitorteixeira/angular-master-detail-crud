221e58285085fff59ef41d4d697981ebac85b407
Heitor Teixeira <heitor.teixeira@hotmail.com>
2019-05-28 19:08:00 -0300

    adding categories module

---
+++ src/app/pages/categories/category-form/category-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-category-form',
+  templateUrl: './category-form.component.html',
+  styles: []
+})
+export class CategoryFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
